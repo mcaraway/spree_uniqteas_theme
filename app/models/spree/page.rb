3de3ab204406ff04b6d575b7d3e203bf78b7e6d7
Michael Caraway <michael@carawaytea.com>
2013-07-30 08:53:07 -0400

    added social icons

--- app/models/spree/page.rb
+++ app/models/spree/page.rb
@@ -1,3 +1,4 @@
 class Spree::Page < ActiveRecord::Base
-  attr_accessible :html, :meta_description, :meta_keywords, :name, :page_title, :permalink, :search_keywords
+  attr_accessible :html, :meta_description, :meta_keywords, :name, :page_title, 
+                  :permalink, :search_keywords, :visible
 end
