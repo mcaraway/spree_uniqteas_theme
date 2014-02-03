b177d7d4e87b56d39204b212e599a4eaf732abf2
Michael Caraway <michael@carawaytea.com>
2014-02-03 14:24:27 -0500

    updated to work with spree 2.1

--- app/overrides/spree/shared/_head/insert_viewport_meta.rb
+++
@@ -1,5 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/shared/_head", 
-                     :name => "insert_viewport_meta", 
-                     :insert_after => "title", 
-                     :text => "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">",
-                     :original => '75558c0e351bb55a748dae3078dc9b4e6abbacb0')
